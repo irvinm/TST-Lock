1536387af4edeb0a6a19cb6f3aca8b2f75408df2
irvinm <irvinm@yahoo.com>
2020-06-24 16:17:38 -0400

    Added winrar to include new "images" folder

--- MakeXPI.bat
+++ MakeXPI.bat
@@ -1,3 +1,4 @@
 del TST-Lock.xpi
-"C:\Program Files\WinRAR\winrar.exe" a TST-Lock.zip *.*
+"C:\Program Files\WinRAR\winrar.exe" a TST-Lock.zip background.js manifest.json
+"C:\Program Files\WinRAR\winrar.exe" a -r TST-Lock.zip images
 ren TST-Lock.zip TST-Lock.xpi
